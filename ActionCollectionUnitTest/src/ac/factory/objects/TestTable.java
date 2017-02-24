c7bad24b6721c8e8d2d6715556960959a85f56f5
ssdhaliwal <serajdhaliwal@gmail.com>
2017-02-24 17:44:01 -0500

    updated for other files

---
+++ ActionCollectionUnitTest/src/ac/factory/objects/TestTable.java
@@ -0,0 +1,21 @@
+package ac.factory.objects;
+
+import elsu.events.*;
+import ac.core.*;
+import elsu.database.*;
+import elsu.support.*;
+
+public class TestTable extends ActionObject {
+
+    public TestTable(ConfigLoader config, DatabaseManager dbManager) throws Exception {
+        // retrieve the action configuration from the actionobject config for
+        // the class name
+        super(config, dbManager);
+    }
+
+    @Override
+    public Object EventHandler(Object sender, IEventStatusType status, String message, Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
